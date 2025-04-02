c6375d7450c4fe849fab28e14a72dd42d07a871c
Esboriol <otavioesboriol@gmail.com>
2025-04-02 16:52:13 -0300

    py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, redirect, request
 from models.chamado import *
 from database import get_db_connection
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+import os
 
+UPLOADS_PHOTO = 'photos'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOADS_PHOTO
+
+@app.route('/photos/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(UPLOADS_PHOTO, filename)
 
 @app.route("/")
 def inicial():
@@ -81,12 +90,19 @@ def form():
 
 @app.route("/salvarChamado", methods=["POST"])
 def salvarChamado():
-    email_solicitante = request.form["email_requisitante"]
-    nome_solicitante = request.form["nome_requisitante"]
-    ambiente = request.form["ambiente"]
-    descricao_chamado = request.form["descricao_chamado"]
-    salvar(nome_solicitante, email_solicitante, ambiente, descricao_chamado)
-    return render_template("obrigado.html")
+    if 'photo_post' in request.files:
+
+        photo = request.files['photo_post']
+        photo_name = secure_filename(photo.filename)
+        photo_path = os.path.join(UPLOADS_PHOTO, photo_name)
+        photo.save(photo_path)
+
+        email_solicitante = request.form["email_requisitante"]
+        nome_solicitante = request.form["nome_requisitante"]
+        ambiente = request.form["ambiente"]
+        descricao_chamado = request.form["descricao_chamado"]
+        salvar(nome_solicitante, email_solicitante, ambiente, descricao_chamado, photo_path)
+        return render_template("obrigado.html")
 
 @app.route("/login", methods=["POST"])
 def login():
--- models/chamado.py
+++ models/chamado.py
@@ -3,7 +3,7 @@ from typing import List
 
 
 class Chamado:
-    def __init__(self, chamado_id, titulo, descricao, data, status, local, observacao, nome_solicitante, email_solicitante):
+    def __init__(self, chamado_id, titulo, descricao, data, status, local, observacao, nome_solicitante, email_solicitante, Photo_path):
         self.chamado_id = chamado_id
         self.titulo = titulo
         self.descricao = descricao
@@ -13,25 +13,27 @@ class Chamado:
         self.observacao = observacao
         self.nome_solicitante = nome_solicitante
         self.email_solicitante = email_solicitante
+        self.Photo_path = Photo_path
 
 def get_chamados_by_status(status) -> List[Chamado]:
     conn = get_db_connection()
     cursor = conn.cursor()
 
+    # Alterando para incluir o campo photo_path na consulta
     if status == None:
-        cursor.execute('SELECT id, titulo, descricao, data, status, local, observacao FROM chamados;')
+        cursor.execute('SELECT id, titulo, descricao, data, status, local, observacao, photo_path FROM chamados;')
     elif status == 'ongoing':
-        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao FROM chamados WHERE status = 'ongoing';")
+        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao, photo_path FROM chamados WHERE status = 'ongoing';")
     elif status == 'finished':
-        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao FROM chamados WHERE status = 'finished';")
+        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao, photo_path FROM chamados WHERE status = 'finished';")
     elif status == 'most-urgent':
-        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao FROM chamados WHERE status = 'most-urgent';")
+        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao, photo_path FROM chamados WHERE status = 'most-urgent';")
     elif status == 'urgent':
-        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao FROM chamados WHERE status = 'urgent';")
+        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao, photo_path FROM chamados WHERE status = 'urgent';")
     elif status == 'common':
-        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao FROM chamados WHERE status = 'common';")
+        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao, photo_path FROM chamados WHERE status = 'common';")
     elif status == 'no-status':
-        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao FROM chamados WHERE status = 'no-status';")
+        cursor.execute("SELECT id, titulo, descricao, data, status, local, observacao, photo_path FROM chamados WHERE status = 'no-status';")
 
     results = cursor.fetchall()
 
@@ -54,7 +56,8 @@ def get_chamados_by_status(status) -> List[Chamado]:
             local=result[5],
             observacao=observacao,
             nome_solicitante="",
-            email_solicitante=""
+            email_solicitante="",
+            Photo_path=result[7]  # Atualizando para pegar o photo_path
         )
         chamados.append(chamado)
     return chamados
@@ -72,15 +75,15 @@ def POSTChamado(chamado_id, observacao, status):
     conn.commit()
     conn.close()
 
-def salvar(nome, email, ambiente, descricao):
+def salvar(nome, email, ambiente, descricao, photo_path):
     conn = get_db_connection()
     cursor = conn.cursor()
     from datetime import datetime
     data_hora_atual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     cursor.execute(
         """
-        INSERT into chamados (nome_solicitante, email_solicitante, local, descricao, titulo, data, status) VALUES (%s, %s, %s,%s, %s, %s, %s);
-        """, (nome, email, ambiente, descricao, "Chamado", data_hora_atual, "no-status")
+        INSERT into chamados (nome_solicitante, email_solicitante, local, descricao, titulo, data, status, photo_path) VALUES (%s, %s, %s,%s, %s, %s, %s, %s);
+        """, (nome, email, ambiente, descricao, "Chamado", data_hora_atual, "no-status", photo_path)
     )
     conn.commit()
     conn.close()
